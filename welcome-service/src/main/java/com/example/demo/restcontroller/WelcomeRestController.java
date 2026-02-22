5eb83965c0b12703728d60085d0970ef64a5e99c
makkar-vineet <vmakkar64@gmail.com>
2026-02-22 23:50:52 +0530

    load balancer

--- welcome-service/src/main/java/com/example/demo/restcontroller/WelcomeRestController.java
+++ welcome-service/src/main/java/com/example/demo/restcontroller/WelcomeRestController.java
@@ -1,14 +1,23 @@
 package com.example.demo.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
 	
+	@Autowired
+	private Environment env;
+	
+	
+	
 	@GetMapping("/welcome")
 	public String getWelcomeMsg() {
-		String msg="code practice....";
+		String port =env.getProperty("server.port");
+		
+		String msg="code practice...." + "running on port ( " +port+" )";
 				return msg;
 	}
 
